1a3aea89e9b04d4096d6b94293a763a7af7544fa
Mick van Gelderen <mick@mick-N61Jv.(none)>
2013-05-06 22:16:37 +0200

    First load of snippets

---
+++ examples/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -f -v *.pdf *.swp *.acn *.acr *.alg *.aux *.bbl *.blg *.dvi *.fdb_latexmk 
+rm -f -v *.glg *.glo *.gls *.idx *.ilg *.ind *.ist *.lof *.log *.lot *.maf *.mtc 
+rm -f -v *.mtc0 *.nav *.nlo *.out *.pdfsync *.ps *.snm *.synctex.gz *.toc *.vrb *.xdy *.tdo
